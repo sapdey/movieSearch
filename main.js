72a1457fad7fffa4628ea9520d41e2e334b9cd77
sapdey <saptarshidey13@gmail.com>
2017-03-27 15:43:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText) {
-	axios.get('http://www.omdbapi.com?s='+searchText)
+	axios.get('https://www.omdbapi.com?s='+searchText)
 	.then((response) => {
 		console.log(response);
 		let movies = response.data.Search;
@@ -40,7 +40,7 @@ function movieSelected(id) {
 function getMovie(){
 	let movieId = sessionStorage.getItem('movieId');
 
-	axios.get('http://www.omdbapi.com?i='+movieId)
+	axios.get('https://www.omdbapi.com?i='+movieId)
 	.then((response) => {
 		console.log(response);
 		let movie = response.data;
@@ -68,7 +68,7 @@ function getMovie(){
 					<h3>Plot</h3>
 					${movie.Plot}
 					<hr />
-					<a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+					<a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
 					<a href="index.html" class="btn btn-default">Go back</a>
 				</div>
 			</div>
