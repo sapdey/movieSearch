2feb9222751406826e0fc7aa7faa6573a98f682d
Saptarshi Dey <saptarshidey13@gmail.com>
2018-07-31 21:30:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText) {
-	axios.get('https://www.omdbapi.com?s='+searchText)
+	axios.get('https://www.omdbapi.com?s='+searchText+'&apikey=2c549349')
 	.then((response) => {
 		console.log(response);
 		let movies = response.data.Search;
@@ -40,7 +40,7 @@ function movieSelected(id) {
 function getMovie(){
 	let movieId = sessionStorage.getItem('movieId');
 
-	axios.get('https://www.omdbapi.com?i='+movieId)
+	axios.get('https://www.omdbapi.com?i='+movieId+'&apikey=2c549349')
 	.then((response) => {
 		console.log(response);
 		let movie = response.data;
